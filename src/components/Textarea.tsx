d4aa6f1c641691449e0881147c65e02e21d95dfa
kondo0602 <kondo8363@gmail.com>
2023-10-01 19:19:36 +0900

    feat: textareaにスタイルを追加

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -2,24 +2,39 @@ import { useState, type ChangeEvent } from 'react';
 import { useDebounce } from 'ahooks';
 import { formatTextAsDirectoryTree } from './hooks/formatTextAsDirectoryTree';
 
+const defaultValue = `/
+ src
+  components
+   Textarea.tsx
+  pages
+   index.astro
+ .gitignore
+ astro.config.mjs
+ package.json
+ package.lock.json
+ tsconfig.json
+`;
+
 export const Textarea = () => {
-  const [inputText, setInputText] = useState<string>('');
+  const [inputText, setInputText] = useState<string>(defaultValue);
   const debouncedValue = useDebounce(inputText, { wait: 500 });
   const formattedText = formatTextAsDirectoryTree(debouncedValue);
 
   return (
     <div className="flex justify-center w-full p-8 gap-8">
       <textarea
-        className="flex-auto h-96 px-4 py-2 border font-mono"
+        className="p-2.5 flex-auto h-96 text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 font-mono"
         value={inputText}
         onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
           setInputText(e.target.value)
         }
+        spellCheck="false"
       />
       <textarea
-        className="flex-auto h-96 px-4 py-2 border font-mono"
+        className="flex-auto h-96 p-2.5 text-sm text-gray-900 rounded-lg border border-gray-300 font-mono"
         value={formattedText}
         disabled
+        spellCheck="false"
       />
     </div>
   );
