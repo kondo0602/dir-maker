26f2eb81a2f5b32b41193a8e8370f48799839359
kondo0602 <kondo8363@gmail.com>
2023-10-01 20:31:22 +0900

    feat: 出力された内容をコピーするボタンを追加

---
+++ src/components/CopyButton.tsx
@@ -0,0 +1,38 @@
+import type { ComponentProps, FC } from 'react';
+import clsx from 'clsx';
+
+type CopyButtonProps = ComponentProps<'button'> & {
+  text: string;
+};
+
+export const CopyButton: FC<CopyButtonProps> = ({ className, text }) => {
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+  };
+
+  return (
+    <button
+      type="button"
+      className={clsx(
+        className,
+        ' text-gray-700 border border-gray-700 hover:bg-gray-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm p-2.5 text-center items-center'
+      )}
+      onClick={handleCopy}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+        />
+      </svg>
+    </button>
+  );
+};
--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,6 +1,7 @@
 import { useState, type ChangeEvent } from 'react';
 import { useDebounce } from 'ahooks';
 import { formatTextAsDirectoryTree } from './hooks/formatTextAsDirectoryTree';
+import { CopyButton } from './CopyButton';
 
 const defaultValue = `/
  src
@@ -21,17 +22,21 @@ export const Textarea = () => {
   const formattedText = formatTextAsDirectoryTree(debouncedValue);
 
   return (
-    <div className="flex justify-center w-full p-8 gap-8">
+    <div className="grid grid-cols-2 gap-x-8 gap-y-2 w-full px-8">
       <textarea
-        className="flex-auto h-96 p-2.5 font-mono text-sm text-gray-900 border border-gray-300 rounded-lg whitespace-pre overflow-auto focus:ring-blue-500 focus:border-blue-500 "
+        className="col-start-1 col-span-1 row-start-2 row-span-2 h-96 p-2.5 font-mono text-sm text-gray-900 border border-gray-300 rounded-lg whitespace-pre overflow-auto focus:ring-blue-500 focus:border-blue-500"
         value={inputText}
         onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
           setInputText(e.target.value)
         }
         spellCheck="false"
       />
+      <CopyButton
+        className="col-start-2 col-span-1 row-start-1 justify-self-end"
+        text={formattedText}
+      />
       <textarea
-        className="flex-auto h-96 p-2.5 font-mono text-sm text-gray-900 border border-gray-300 rounded-lg whitespace-pre overflow-auto"
+        className="col-start-2 col-span-1 row-start-2 h-96 p-2.5 font-mono text-sm text-gray-900 border border-gray-300 rounded-lg whitespace-pre overflow-auto"
         value={formattedText}
         disabled
         spellCheck="false"
