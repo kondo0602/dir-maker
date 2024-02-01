359579a98d3e2d251d0a91744a3d26a52203a1c6
kondo0602 <kondo8363@gmail.com>
2024-02-01 22:00:25 +0900

    feat: 複数行の`Tab`インデントに対応

--- src/components/hooks/useKeyDown.ts
+++ src/components/hooks/useKeyDown.ts
@@ -1,3 +1,5 @@
+const getLines = (text: string, position: number) => text.substring(0, position).split('\n');
+
 export const useKeyDown = () => {
   const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     const { value, selectionStart, selectionEnd } = e.currentTarget;
@@ -5,10 +7,34 @@ export const useKeyDown = () => {
     if (e.key === "Tab") {
       e.preventDefault();
 
-      const newValue = value.substring(0, selectionStart) + " " + value.substring(selectionEnd);
-      e.currentTarget.value = newValue;
-      e.currentTarget.selectionStart = selectionStart + 1;
-      e.currentTarget.selectionEnd = selectionEnd + 1;
+      if (selectionStart === selectionEnd) {
+        const newValue = value.substring(0, selectionStart) + " " + value.substring(selectionEnd);
+        e.currentTarget.value = newValue;
+        e.currentTarget.selectionStart = selectionStart + 1;
+        e.currentTarget.selectionEnd = selectionEnd + 1;
+      } else {
+        const linesBeforeCaret = getLines(value, selectionStart);
+        const startLine = linesBeforeCaret.length - 1;
+        const endLine = getLines(value, selectionEnd).length - 1;
+        const startLineText = linesBeforeCaret[startLine];
+
+        e.currentTarget.value = value
+          .split('\n')
+          .map((line, i) => {
+            if (i >= startLine && i <= endLine) {
+              return " " + line;
+            }
+
+            return line;
+          })
+          .join('\n');
+
+        e.currentTarget.selectionStart = startLineText && /\S/.test(startLineText)
+        ? selectionStart + 1
+        : selectionStart;
+
+        e.currentTarget.selectionEnd = selectionEnd + 1 * (endLine - startLine + 1)
+      }
     }
   };
 
