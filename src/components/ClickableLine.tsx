07c09287988fa41aebc5fabb2a6055395ae1be3f
ShuntaToda <todashuntawork@gmail.com>
2024-08-22 19:22:27 +0900

    npm run check

--- src/components/ClickableLine.tsx
+++ src/components/ClickableLine.tsx
@@ -8,17 +8,28 @@ type ClickableLineProps = {
 	onClick: (dirData: Directory) => void;
 };
 
-export const ClickableLine: FC<ClickableLineProps> = ({ dirIndex, directories, onClick }) => {
-  const {prefixes, dirData} =  generateDirectoryLineData(directories,dirIndex)
-  
-  return (
-    <div className="flex">
-      {prefixes.map((prefix, index) => (
-        <div key={`${dirData.dirName}-${index}`}>{prefix}</div>
-      ))}
-      <div className="cursor-pointer hover:bg-slate-200 rounded" onClick={() => onClick(dirData)}>
-        {dirData.dirName}
-      </div>
-    </div>
-  )
-}
+export const ClickableLine: FC<ClickableLineProps> = ({
+	dirIndex,
+	directories,
+	onClick,
+}) => {
+	const { prefixes, dirData } = generateDirectoryLineData(
+		directories,
+		dirIndex,
+	);
+
+	return (
+		<div className="flex">
+			{prefixes.map((prefix, index) => (
+				<div key={`${dirData.dirName}-${index}`}>{prefix}</div>
+			))}
+			<button
+				className="cursor-pointer hover:bg-slate-200 rounded"
+				onClick={() => onClick(dirData)}
+				type="button"
+			>
+				{dirData.dirName}
+			</button>
+		</div>
+	);
+};
